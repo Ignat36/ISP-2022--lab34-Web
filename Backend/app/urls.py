6d4e38b4da90eeb3428a32d7ec2ab0152c700560
Ignat <sharignat9@gmail.com>
2022-05-20 01:04:06 +0300

    Understend everything, ready to steal a lot of html and css

--- Backend/app/urls.py
+++ Backend/app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='news-index'),
+    path('about/', views.about, name='news-about')
 ]
--- Backend/app/views.py
+++ Backend/app/views.py
@@ -1,9 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 import requests
 
 # Create your views here.
 def index(request):
-    r = requests.get('http://api.mediastack.com/v1/news?access_key=07f04176b5cab9b8543438d76220d466&countries=by,ru&languages=en,ru')
+    r = requests.get('http://api.mediastack.com/v1/news?access_key=07f04176b5cab9b8543438d76220d466&countries=ru,us&languages=ru')
     result = r.json()
     data = result['data']
     title = []
@@ -21,3 +22,6 @@ def index(request):
     # news = [(t1,d1,i1,u1), (t2,d2,i2,u2)]
 
     return render(request, 'app/index.html', {'news': news})
+
+def about(request):
+    return HttpResponse('<h1>Group 053502 | Shargorodsky Ignat | Lab 3-4 </h1>News site which loads basic news from api https://mediastack.com/.<br>Site has 4 different roles Reader, Pudlisher, Edditor and Admin<br>Readers can leave comments under news.')
