dd98c4c483d9afb2b2dc538c2d74d3f072869a15
Ignat <sharignat9@gmail.com>
2022-05-25 02:48:14 +0300

    Deploy version 1.25

--- ISP-2022--lab34-Web/settings.py
+++ ISP-2022--lab34-Web/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +140,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DEFAULT_ROMA = 'https://live.staticflickr.com/65535/52094090018_2b71027e14_q.jpg'
+
+django_heroku.settings(locals())
