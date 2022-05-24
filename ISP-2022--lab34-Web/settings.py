85babdfd9717c149d6cc5c198e41215732b8cd65
Ignat <sharignat9@gmail.com>
2022-05-25 01:56:31 +0300

    Deploy version 1.22

--- ISP-2022--lab34-Web/settings.py
+++ ISP-2022--lab34-Web/settings.py
@@ -21,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6p_51*hd4r(t9za10+!3=al_c!xl+yr#6mhg6^6kdvhun2psph'
+# SECRET_KEY = 'django-insecure-6p_51*hd4r(t9za10+!3=al_c!xl+yr#6mhg6^6kdvhun2psph'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'newspetproject.herokuapp.com'
+]
 
 # Application definition
 
