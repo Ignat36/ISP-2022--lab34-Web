52660eac1d3bd2a82058cf51bed66c52d19d6e40
Ignat <sharignat9@gmail.com>
2022-05-25 02:16:51 +0300

    Deploy version 1.24

--- ISP-2022--lab34-Web/settings.py
+++ ISP-2022--lab34-Web/settings.py
@@ -135,4 +135,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'news-index'
 LOGIN_URL = 'login'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 DEFAULT_ROMA = 'https://live.staticflickr.com/65535/52094090018_2b71027e14_q.jpg'
